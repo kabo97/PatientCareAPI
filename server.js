49604d9e02ac4cb5ae368e729e6e54c4881f60af
kabo97 <alielmatarawy8@gmail.com>
2024-11-02 21:30:37 +0300

    Step 2 (final)

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
@@ -15,12 +15,16 @@ app.use(express.json());
 const patientsRoutes = require('./routes/patients');
 const usersRoutes = require('./routes/auth');
 const prescriptionsRoutes = require('./routes/prescription');
+const pharmacistRoutes = require('./routes/pharmacists');
+const notifyRoutes = require('./routes/notification');
 
 // Use routes
-app.get('/', (req, res) => res.send('Hello from Express on Vercel!'));
+app.get('/', (req, res) => res.send('Hello from Server'));
 app.use('/api/patients', patientsRoutes);
 app.use('/api/prescriptions', prescriptionsRoutes);
 app.use('/api/auth', usersRoutes);
+app.use('/api/pharmacist', pharmacistRoutes);
+app.use('/api/queue/pickup/notify', notifyRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);

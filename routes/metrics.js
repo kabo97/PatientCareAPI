f53c8de7064d8ce91a7f284a4c93251bb50bd2f2
kabo97 <alielmatarawy8@gmail.com>
2024-12-16 12:39:35 +0300

    teraform

--- routes/metrics.js
+++ routes/metrics.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const sql = neon(process.env.DATABASE_URL);
 // GET /metrics endpoint
-router.get('/', async (req, res) => {
+router.get('/system', async (req, res) => {
     try {
         const metrics = await sql`SELECT * FROM metrics ORDER BY timestamp DESC LIMIT 10`;
         res.status(200).json(metrics);
@@ -14,5 +14,13 @@ router.get('/', async (req, res) => {
         res.status(500).json({ error: 'Failed to fetch metrics' });
     }
 });
-
+router.get('/prescription', async (req, res) => {
+    try {
+        const metrics = await sql`SELECT * FROM prescriptions`;
+        res.status(200).json(metrics);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to fetch metrics' });
+    }
+});
 module.exports = router;

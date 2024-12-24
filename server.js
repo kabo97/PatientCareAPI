bb23196a61cbb195667d33261993270a0ce881a3
MR-Alyousif <mryousif967@gmail.com>
2024-12-24 20:56:52 +0300

    Add queue update handling and error middleware to server

--- server.js
+++ server.js
@@ -24,6 +24,13 @@ app.use(express.json());
 io.on('connection', (socket) => {
     console.log('Client connected:', socket.id);
 
+    // Handle queue updates
+    socket.on('queueUpdate', (update) => {
+        console.log('Queue update received:', update);
+        // Broadcast the update to all connected clients
+        io.emit('queueUpdate', update);
+    });
+
     socket.on('disconnect', () => {
         console.log('Client disconnected:', socket.id);
     });
@@ -39,6 +46,7 @@ const prescriptionsRoutes = require('./routes/prescription');
 const medicinesRoutes = require('./routes/medicines');
 const pharmacistRoutes = require('./routes/pharmacists');
 const metricsRoutes = require('./routes/metrics');
+const queueRoutes = require('./routes/queue');
 
 // Use routes
 app.get('/', (req, res) => res.send('Hello from Server'));
@@ -48,7 +56,15 @@ app.use('/api/auth', usersRoutes);
 app.use('/api/pharmacists', pharmacistRoutes);
 app.use('/api/metrics', metricsRoutes);
 app.use('/api/medicines', medicinesRoutes);
+app.use('/api/queue', queueRoutes);
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Something broke!' });
+});
+
+// Start server
 httpServer.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
